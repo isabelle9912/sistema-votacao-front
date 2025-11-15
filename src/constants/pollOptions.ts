ece438e9e17666fdf9710348dfa00f75db568235
isa9912 <isabellesaahirah28@gmail.com>
2025-11-15 14:39:26 -0300

    feat: add the pollOptions.ts file

---
+++ src/constants/pollOptions.ts
@@ -0,0 +1,26 @@
+export const pollOptions = [
+  {
+    id: "golang",
+    name: "Go (Backend)",
+    imageUrl: "/mascots/gopher.png",
+    color: "#00ADD8",
+  },
+  {
+    id: "react",
+    name: "React (Frontend)",
+    imageUrl: "/mascots/react-ninja.png",
+    color: "#61DAFB",
+  },
+  {
+    id: "redis",
+    name: "Redis (Cache/Fila)",
+    imageUrl: "/mascots/redis.png",
+    color: "#DC382D",
+  },
+  {
+    id: "cassandra",
+    name: "Cassandra (Banco)",
+    imageUrl: "/mascots/cassandra-eye.png",
+    color: "#1287B1",
+  },
+];
