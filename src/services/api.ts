e1d1fc54c98e2489f84f68c3e0beb6271050b83c
isa9912 <isabellesaahirah28@gmail.com>
2025-11-19 13:02:55 -0300

    feat(api): adds getCandidates and postCandidates services

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,14 +3,32 @@ import { getApiBaseUrl } from "../helpers/baseUrl";
 
 const API_URL = getApiBaseUrl();
 
+export const getCandidates = async () => {
+  const response = await fetch(`${API_URL}/candidatos`);
+  return await response.json();
+};
+
+export const postCandidates = async (optionName: string) => {
+  await fetch(`${API_URL}/candidatos`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nome: optionName }),
+  });
+};
+
 export const getVotes = async () => {
-  const response = await fetch(`${API_URL}/votes`);
+  const response = await fetch(`${API_URL}/votos`);
   return await response.json();
 };
 
 export const postVote = async (optionId: string) => {
-  await fetch(`${API_URL}/votes`, {
+  await fetch(`${API_URL}/votos`, {
     method: "POST",
-    body: JSON.stringify({ optionId }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ candidato_id: optionId }),
   });
 };
